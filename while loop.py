f7a0d29a85c8a356ebde30f0ff2655ccf86daa99
kirana <kiranakiru7777@gmail.com>
2026-04-02 21:46:14 +0530

    practice update

--- while loop.py
+++ while loop.py
@@ -1,9 +1,9 @@
-#while loop
+#while loop 
 
 is_faild = True
 i = 1
 while is_faild :#and i<=100:
-    if i%2!=0: #is not even 
+    if i%2 == 0: #is not even 
        i= i + 1
        continue
     print(f"try{i}")
@@ -22,5 +22,33 @@ while i<=10:
     i +=1
 
     
+#nested while loops
 
-   
+w = 0
+
+while w <=10:
+    x = 0
+    while x<w:
+        print("kiran", end="_")
+        x+=1
+    print("")
+    w +=1
+ 
+ #ATM PIN
+
+pin = "1234"
+trials = 1
+
+while trials<=3:
+    input_pin = input(f"Trail-{trials} | PIN :")
+    trials +=1
+    if input_pin == pin:
+        print("CORRECT")
+        break
+    else:
+        print("INCORRECT")
+    
+
+
+
+
