c82d15cb06eb7aa305f21287526e10449204bfff
kirana <kiranakiru7777@gmail.com>
2026-04-08 23:09:11 +0530

    practice update

--- comprehension.py
+++ comprehension.py
@@ -5,3 +5,9 @@ dubled = []
 for num in number:
     dubled .append(num*2)
     print(dubled)
+
+#looping through a dictionary
+my_dict = {"name":"john","age":30,"city":"new york"}
+for key in my_dict:
+    print(key,my_dict[key])
+    
