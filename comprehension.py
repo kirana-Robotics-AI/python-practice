7e12920241a64ab757b3269ec535b8febbbf5f61
kirana <kiranakiru7777@gmail.com>
2026-04-10 12:51:17 +0530

    auto-commit

--- comprehension.py
+++ comprehension.py
@@ -1,7 +1,6 @@
 #dubling each element in a list
 number = [1,2,3,45]
 dubled = []
-
 for num in number:
     dubled .append(num*2)
     print(dubled)
@@ -11,3 +10,26 @@ my_dict = {"name":"john","age":30,"city":"new york"}
 for key in my_dict:
     print(key,my_dict[key])
     
+#for loop with range()
+
+students = ["kirana","manju","raghu"]
+marks = [70,80,50]
+
+student_marks = {}
+
+for i in range(len(students)):
+    student_marks[students[i]] = marks[i]
+
+print(student_marks)
+
+
+for index, student in enumerate(students):
+    student_marks[student] = marks[index]
+
+print(student_marks)
+
+for i in range(3):
+    student_marks[students[i]] = marks[i]
+
+print(student_marks)
+               
