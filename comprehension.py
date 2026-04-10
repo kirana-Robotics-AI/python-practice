ef3e787ad2a0636ab5e330fa721a88197d376ab6
kirana <kiranakiru7777@gmail.com>
2026-04-10 17:10:18 +0530

    auto-commit

--- comprehension.py
+++ comprehension.py
@@ -17,7 +17,7 @@ marks = [70,80,50]
 
 student_marks = {}
 
-for i in range(len(students)):
+for i in range(1,len(students),2): #step:steep:stop
     student_marks[students[i]] = marks[i]
 
 print(student_marks)
@@ -32,4 +32,43 @@ for i in range(3):
     student_marks[students[i]] = marks[i]
 
 print(student_marks)
-               
+
+
+#List comprehension in For  Loop 
+
+l = [1,2,4,6]
+dl = [item**2 for item in l] #[expresion for item in collection]
+
+print(dl)
+
+m = [n for n in range(1,12)]
+
+ml = [n**2 for n in m if n%2 == 0]
+
+print(ml)
+
+r = ["kirana","mango","apple"]
+print(r)
+
+cl = [x[1] for x in r]
+print(cl)
+
+#dictionary comprrehansion
+
+city = {
+    "bangalore":75,
+    "shimoga":55,
+    "hubli":90
+};
+
+lc = {key:value for key,value in city.items() if value>60}
+
+print(lc)
+
+#letters in a list
+
+names = ["kirana","mango","apple"]
+
+d = {name:len(name) for name in names}
+
+print (d)
